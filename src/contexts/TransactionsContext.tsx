083327f6b58ae443e5123371f5ac5ecde10e0d9f
Henrick <ricknogueira1231@gmail.com>
2025-03-01 14:56:42 -0300

    #D097 20 - Configurando Axios

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transaction {
   id: number;
@@ -25,15 +26,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL("http://localhost:3333/transactions");
-
-    if (query) {
-      url.searchParams.append("q", query);
-    }
-
-    const response = await fetch(url);
-    const data = await response.json();
-    setTransactions(data);
+    const response = await api.get("transactions", {
+      params: {
+        q: query,
+      },
+    });
+    setTransactions(response.data);
   }
 
   useEffect(() => {
@@ -41,9 +39,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   }, []);
 
   return (
-    <TransactionsContext.Provider
-      value={{ transactions, fetchTransactions }}
-    >
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
